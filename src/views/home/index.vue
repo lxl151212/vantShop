51a28089d936472dd34577272c0eaa580873aa8c
htjia <18829351109@163.com>
2020-05-15 23:03:00 +0800

    first commit

---
+++ src/views/home/index.vue
@@ -0,0 +1,47 @@
+<!-- home -->
+<template>
+  <div class="app-container">
+    <!-- <van-cell icon="success" v-for="item in list" :key="item" :title="item" />
+    <van-button type="info" @click="handleSearch">查詢</van-button> -->
+    <div class="time-axis"></div>
+  </div>
+</template>
+
+<script>
+import { getUserName } from '@/api/user'
+
+export default {
+  data() {
+    return {
+      list: ['Vue-cli4']
+    }
+  },
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {
+    handleSearch() {
+      const params = {
+        id: 123,
+        name: '張三'
+      }
+      getUserName(params).then(res => {
+        console.log(res)
+      })
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.app-container {
+  width: 100%;
+
+  .time-axis {
+    width: 100%;
+    height: 4px;
+    background: red;
+  }
+}
+</style>
