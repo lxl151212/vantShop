cfdcfc4a42483628cc5fde60f6440bc9c8f6b094
htjia <18829351109@163.com>
2020-05-20 15:04:55 +0800

    order list

--- src/views/order/list.vue
+++ src/views/order/list.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="list-box">
-    <Header title="确认订单"></Header>
-    <div class="content">订单列表</div>
+    <Header title="拼团订单"></Header>
+    <van-tabs class="tab" @click="tabClick">
+      <van-tab v-for="tab in tabs" :title="tab.name"></van-tab>
+    </van-tabs>
+    <div class="order-container" v-for="(order, index) in orderList" :key="index">
+      <div class="header-container">
+        <div style="font-size: 15px;font-weight: bold;float: left;"><van-icon name="send-gift-o" size="20"/><span style="vertical-align: top">全友家私旗舰店</span></div>
+        <div class="order-type">{{ order.type }}</div>
+      </div>
+      <div class="section-container">
+        <div class="img"></div>
+        <div class="content">
+          <div class="title">{{ order.title }}</div>
+          <div class="describe">{{ order.describe }} <span style="float: right;">x1</span></div>
+          <div class="price"><span class="font-size-samll">￥</span>{{ order.price }}<span class="font-size-samll">.00</span></div>
+          <div class="total">共计{{ order.total }}件商品，小计￥<span class="font-size-large">{{ order.total * order.price }}</span>.00</div>
+          <div class="btn-group">
+            <van-button round type="default" hairline size="small">取消订单</van-button>
+            <van-button round type="danger" size="small">去付款</van-button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -18,19 +39,125 @@ export default {
   },
   data() {
     return {
-      active: 0
+      active: 0,
+      tabs: [
+        { name: '全部', id: '0' },
+        { name: '待付款', id: '1' },
+        { name: '拼团中', id: '2' },
+        { name: '待补价', id: '3' },
+        { name: '待发货', id: '4' },
+        { name: '待评价', id: '5' },
+      ],
+      orderList: [],
+      copyOrderList: [
+        {
+          type: '待付款',
+          title: '现代简约电视柜，小户型可伸缩电视机小型机柜',
+          describe: '黑色, 2m x 1.5m',
+          total: 2,
+          price: 388,
+        },
+        {
+          type: '拼团中',
+          title: '现代简约电视柜，小户型可伸缩电视机小型机柜',
+          describe: '黑色, 2m x 1.5m',
+          total: 2,
+          price: 388,
+        }
+      ]
     }
+  },
+  mounted() {
+    this.orderList = this.copyOrderList
   },
   methods: {
     goBack() {
       history.go(-1)
+    },
+    tabClick(title, name) {
+      if (name === '全部') {
+        this.orderList = this.copyOrderList
+      } else {
+        this.orderList = this.copyOrderList.filter(item =>  name === item.type)
+      }
     }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .list-box >>> .van-tabs__line{
+    background: linear-gradient(
+        -83deg,
+        rgba(255, 211, 35, 0.38) 0%,
+        rgba(255, 87, 4, 1) 100%
+    ) !important;
+  }
+</style>
 <style scoped lang="scss">
 .header {
+}
+.order-container{
+  border-radius: 12px;
+  background: #fff;
+  padding: 15px;
+  overflow: hidden;
+  margin-top: 14px;
+  .header-container{
+    overflow: hidden;
+    .order-type{
+      color: #E8281E;
+      float: right;
+      font-size: 14px;
+    }
+  }
+  .section-container{
+    margin-top: 20px;
+    .img{
+      width: 100px;
+      height: 100px;
+      float: left;
+      background-image: url("../../../static/dsg.jpg");
+      background-repeat: no-repeat;
+      background-size: contain;
+      box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+      border-radius: 10px;
+    }
+    .content{
+      margin-left: 120px;
+      .title{
+        font-size: 16px;
+      }
+      .describe{
+        margin: 11px 0;
+        font-size: 13px;
+        color: #B3B3B3;
+      }
+      .price{
+        font-weight: bold;
+        font-size: 18px;
+        color: #5d5d5d;
+        .font-size-samll{
+          font-size: 12px;
+        }
+      }
+      .total{
+        float: right;
+        font-size: 14px;
+        margin: 15px 0;
+        .font-size-large{
+          font-size: 18px;
+        }
+      }
+    }
+  }
+  .btn-group{
+    float: right;
+    button{
+      padding: 8px 25px;
+      margin-left: 15px;
+    }
+  }
 }
 </style>
